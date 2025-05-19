c9fce5ad9c7d5ad9dee8f5a706b53811669d543a
tirlochan016 <trilochan@crashnova.com>
2025-05-19 17:49:33 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tirlochanarora16/go_tmdb
+
+go 1.24.2
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+
+	fmt.Println("TMDB movies app", os.Getenv("API_KEY"))
+}
